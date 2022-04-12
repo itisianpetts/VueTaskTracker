d1b2b430bbbd2cb6de76d41c685aa1756ccbe971
itisianpetts <itisianpetts@gmail.com>
2022-04-12 14:03:53 +0100

    add form submit

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="add-form">
+  <form @submit="onSubmit" class="add-form">
     <div class="form-control">
       <label>Task</label>
       <input type="text" v-model="text" name="text" placeholder="Add Task" />
@@ -25,6 +25,27 @@ export default {
       day: '',
       reminder: false,
     };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      if (!this.text) {
+        alert('Please add a task!');
+        return;
+      }
+      const newTask = {
+        id: Math.floor(Math.random() * 100000),
+        text: this.text,
+        day: this.day,
+        reminder: this.reminder,
+      };
+
+      this.$emit('add-task', newTask);
+
+      this.text = '';
+      this.day = '';
+      this.reminder = false;
+    },
   },
 };
 </script>
